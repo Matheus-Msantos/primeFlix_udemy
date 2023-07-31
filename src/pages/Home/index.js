44a956ec94b2610d6f1c4d73fa14fc7c759d0c92
Matheus-Msantos <matheusmelosantos10@gmail.com>
2023-07-31 17:41:19 -0300

    feat: Puxando os filmes via API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,28 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { apiMovies } from '../../Services/ApiMovies'
 import './index.css';
 
 function Home() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function loadMovies() {
+      const response = await apiMovies.get('movie/now_playing', {
+        params: {
+          api_key: '62850432ba1c35551e42d884af1327ce',
+          language: 'pt-BR',
+          page: 1
+        }
+      });
+
+      setMovies(response.data);
+    };
+
+    loadMovies();
+  }, [])
+
+  console.log(movies);
   return (
     <>Home</>
   );
