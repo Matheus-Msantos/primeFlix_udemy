566d685a341883606c7816d00d3ff6ac0e13723d
Matheus-Msantos <matheusmelosantos10@gmail.com>
2023-08-02 21:46:48 -0300

    feat: pagina 404

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import Favorites from "./pages/Favorites";
 import Header from "./Components/Header";
+import Page404 from "./pages/Erros/Page404";
 
 function RoutesApp() {
   return (
@@ -13,6 +14,8 @@ function RoutesApp() {
         <Route path="/" element={<Home />} />
         <Route path="/filmes/:id" element={<Movies />} />
         <Route path="/favoritos" element={<Favorites />} />
+
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   );
